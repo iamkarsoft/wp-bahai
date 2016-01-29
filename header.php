a18a4d152d1010bc89237310c6e7e6e3e59f0b4c
Kofi Amoussou <karsoft92@gmail.com>
2016-01-29 22:22:20 +0000

    init

---
+++ header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+
+
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+
+	<link rel="pingback"  href="<?php bloginfo('pingback_url'); ?>">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class();?> >
