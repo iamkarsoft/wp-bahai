9e4654aef2fa1fdcb507ed94545bdcbafeea2a62
Kofi Amoussou <karsoft92@gmail.com>
2016-02-24 05:30:16 +0000

    added featured image thumbnail for blog excerpt and increased hheight of the thumbnail boxes

--- templates/content.php
+++ templates/content.php
@@ -4,11 +4,16 @@
 		<section class="blog-wrapper">
 		  <div class="col-lg-4  col-sm-6">
                   <div class="thumbnail" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+										<?php if(has_post_thumbnail()){
+											the_post_thumbnail('thumbnail');
+										}else{ ?>
+											<img src="..." alt="" class="img-responsive img-thumbnail" />
 
-        
+											<?php } ?>
+
 				<h3 class="post-title"><a href="<?php the_permalink()?>"> <?php the_title(); ?></a></h3>
 				<!-- edit link --><h6><?php edit_post_link('edit','<small class="edit">','</small>') ?> <span><?php echo get_the_date(); ?></span></h6>
-				
+
 				<section class="post-content">
 						<?php the_excerpt(); ?>
 
@@ -17,7 +22,7 @@
 
 				</section>
 
-				
+
 				  </div>
             </div>
 		</section>
