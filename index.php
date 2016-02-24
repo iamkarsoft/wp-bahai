4ebff771089ea35a937975411c7a7b21e5343174
Kofi Amoussou <karsoft92@gmail.com>
2016-02-24 12:09:38 +0000

    styling jumbotron

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 
 <section class="container">
-	<div class="jumbotron">
+	<div class="jumbotron"style="background:#d0b6b5,color:#fafafa">
 	    <?php
           $args = array( 'numberposts' => 1 );
           $lastposts = get_posts( $args );
@@ -10,16 +10,16 @@
             <h4 class="post-title page-header"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
             <?php the_excerpt(); ?>
           <?php endforeach; ?>
-		
+
 	</div>
 </section>
 
 		<section class="container">
-		 
+
 			<?php if(have_posts()): ?>
 				<?php get_template_part('templates/content'); ?>
 			<?php endif ?>
-			
+
 		</section>
 
 		<section class="container">
