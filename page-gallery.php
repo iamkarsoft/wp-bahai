38e45324422fb283767a2720b6e39eec5b0b680a
Kofi Amoussou <karsoft92@gmail.com>
2016-03-20 00:59:17 +0000

    adding gallery custom fields

---
+++ page-gallery.php
@@ -0,0 +1,15 @@
+<?php 
+ /*
+*Template Name: Gallery
+ */
+get_header(); ?>
+
+
+
+			<div class="container"><?php if(have_posts()): ?>
+				<?php get_template_part('templates/content','page-gallery'); ?>
+			<?php endif ?>
+			</div>
+	</section>
+<?php get_footer(); ?>
+
